fbb5515aaef109d1973249a281f57e6b69453f0e
tntktnx <you@example.com>
2016-09-17 15:46:26 +0900

    psql

--- index.js
+++ index.js
@@ -1,26 +1,36 @@
+require('dotenv').config();
+
 var path = require('path')
 var express = require('express');
 var redis = require("redis");
+var session = require('express-session')
+var auth = require('./passport');
+var pg = require('pg');
+
 var app = express();
-var client = redis.createClient();
-
-var key = 'hoge:fuga';
-var value = 'piyo';
-
-// 文字列を保存する
-client.set(key, value, function () {
-  console.log("set");
+var passport = auth.passport;
+var redisClient = redis.createClient();
+var pgClient = new pg.Client("postgres://postgres:password@localhost:5432/test1");
+var pgData = [];
+pgClient.connect(function (err) {
+  if (err) return pgData.push(["could not connect to postgres", err]);
+  pgClient.query('SELECT NOW() AS "theTime"', function (err, result) {
+    if (err) return pgData.push(["error running query", err]);
+    console.log(result.rows[0].theTime);
+    pgData.push("success");
+  });
 });
+
+
+app.use(passport.initialize()); 
+app.use(passport.session()); 
+app.use(session({secret: process.env.SECRET}));
 
 app.listen(process.env.PORT || 8000, function () {
   console.log('Node app is running');
 });
 
 app.all("/", function (req, res) {
-  client.get(key, function (err, val) {
-    if (err) return console.log(err);
-    console.log(val);
-  });
   res.sendFile(path.join(__dirname + "/index.html"));
 });
 
@@ -37,8 +47,28 @@ app.all("/matches", function (req, res) {
 });
 
 app.all("/places/:id/:code", function (req, res) {
-  console.log(req.params);
   res.send(JSON.stringify({
-	state: "ok",
+  	state: "ok",
   }));
+});
+
+app.get('/auth/twitter', passport.authenticate('twitter'));
+app.get('/auth/twitter/callback', passport.authenticate(
+  'twitter',
+  { successRedirect: '/auth/test', failureRedirect: '/auth/test' }
+));
+
+
+app.all("/test/auth", function (req, res) {
+  if (passport.session && passport.session.id) {
+    console.log("session ok");
+    res.send(JSON.stringify(passport.session.id));
+  } else {
+    console.log("session ng");
+    res.send("ng");
+  }
+})
+
+app.all("/test/psql", function (req, res) {
+  res.send(JSON.stringify(pgData));
 })
---
+++ passport.js
@@ -0,0 +1,35 @@
+require('dotenv').config()
+
+var TWITTER_CONSUMER_KEY = process.env.TWITTER_CONSUMER_KEY;
+var TWITTER_CONSUMER_SECRET = process.env.TWITTER_CONSUMER_SECRET;
+var passport = require('passport')
+  , TwitterStrategy = require('passport-twitter').Strategy;
+
+// Sessionの設定
+// http://passportjs.org/guide/configure/
+passport.serializeUser(function(user, done) {
+    done(null, user.id);
+});
+passport.deserializeUser(function(obj, done) {
+    done(null, obj);
+});
+
+passport.use(new TwitterStrategy({
+    consumerKey: TWITTER_CONSUMER_KEY,
+    consumerSecret: TWITTER_CONSUMER_SECRET,
+    callbackURL: "http://localhost:8000/auth/twitter/callback"
+  },
+  function(token, tokenSecret, profile, done) {
+    passport.session.id = profile.id;
+
+    // tokenとtoken_secretをセット
+    profile.twitter_token = token;
+    profile.twitter_token_secret = tokenSecret;
+
+    process.nextTick(function () {
+        return done(null, profile);
+    });
+  }
+));
+
+module.exports = {passport: passport};  
