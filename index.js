1173af2c228629e51ab6bb320de2cc78064f9b69
moajo <mimirosiasd@gmail.com>
2016-09-17 12:52:50 +0900

    test

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
-// var express = require('express');
-// var app = express();
-//
-// app.listen(process.env.PORT || 8000, function () {
-//   console.log('Node app is running');
-// });
-console.log("aaaa")
+var express = require('express');
+var app = express();
+
+app.listen(process.env.PORT || 8000, function () {
+  console.log('Node app is running');
+});
