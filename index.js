1095212e7599d452c7c5dce3b4997d3ee8b64756
tntktnx <you@example.com>
2016-09-17 13:16:14 +0900

    html

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
+var path = require('path')
 var express = require('express');
+
 var app = express();
 
+app.listen(process.env.PORT || 8000, function () {
+  console.log('Node app is running');
+});
+
 app.get("/", function (req, res) {
-  res.send("hello node world!");
+  res.sendFile(path.join(__dirname + "/index.html"));
 });
