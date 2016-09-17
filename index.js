e8803d4ac52197393b9bc9720c60f5a94ab75d75
moajo <mimirosiasd@gmail.com>
2016-09-17 12:58:18 +0900

    hello

--- index.js
+++ index.js
@@ -4,3 +4,7 @@ var app = express();
 app.listen(process.env.PORT || 8000, function () {
   console.log('Node app is running');
 });
+
+app.get("/", function (req, res) {
+  res.send("hello node world!");
+})
